749ecf948c1af12d919b7411d76a1fa557a1a694
Hemant Vinchur <hemantvinchur96@gmail.com>
2019-08-05 17:53:48 +0530

    Updated code

--- services/userServices.js
+++ services/userServices.js
@@ -1,44 +1,10 @@
 const functions = require('../function')
 const mongoose = require('mongoose')
 const mongodb = require('mongodb');
-const announcement = require('../models/announcement')
 const email = require('../models/email')
-const notice = require('../models/notice')
 var nodemailer = require('nodemailer');
 const validator = require('../validators/userValidator')
 const jwt = require('jsonwebtoken');
-
-//Create announcement
-
-const userAnnouncement = async (payLoad) => {
-    console.log("Create announcement")
-    try {
-        console.log("userAnnouncement")
-        console.log(payLoad)
-        let userData = await announcement.create(payLoad);
-        return userData
-    } catch (error) {
-        console.error(error)
-        throw error;
-    }
-}
-
-//Create notice
-
-const userNotice = async (payLoad) => {
-    console.log("Create notice")
-    try {
-        console.log("userNotice")
-        console.log(payLoad)
-        let userData = await notice.create(payLoad);
-        return userData
-    } catch (error) {
-        console.error(error)
-        throw error;
-    }
-}
-
-
 //Create email
 
 const userEmail = async (payLoad) => {
@@ -54,7 +20,7 @@ const userEmail = async (payLoad) => {
     }
 }
 
- 
+
 //Send email
 const sendMail = async (payLoad) => {
     try {
@@ -68,9 +34,9 @@ const sendMail = async (payLoad) => {
 
         const mailOptions = {
             from: 'venus.bityotta@gmail.com',
-            to: payLoad.email,
+            to: 'hemantvinchur96@gmail.com',
             subject: payLoad.subject,
-            text: "Hi, "+"\n"+"Client name-: "+payLoad.name+'\n'+"Email-: "+payLoad.email+"\n"+"Message-:"+payLoad.message
+            text: "Hi, " + "\n" + "Client name-: " + payLoad.name + '\n' + "Email-: " + payLoad.email + "\n" + "Message-:" + payLoad.message
         };
 
         let sendMail = await transporter.sendMail(mailOptions);
@@ -82,111 +48,5 @@ const sendMail = async (payLoad) => {
         throw error;
     }
 }
-//Get announcement
 
-const getAnnouncement = async () => {
-    console.log("Get announcement")
-    try {
-        console.log("getAnnouncement")
-        let userData = await announcement.find();
-        return userData;
-    } catch (error) {
-        console.error(error)
-        throw error;
-    }
-}
-
-//Get notice
-
-const getNotice = async () => {
-    console.log("Get notice")
-    try {
-        console.log("getNotice")
-        let userData = await notice.find();
-        return userData;
-    } catch (error) {
-        console.error(error)
-        throw error;
-    }
-}
-
-//Update announcement
-
-const updateAnnouncement = async (payLoad, params) => {
-    console.log("Update announcement")
-    try {
-        console.log("updateAnnouncement")
-        announcement.findOne({ id: params.id }, async (err, data) => {
-
-            console.log('111000')
-            let userData = await announcement.updateOne({
-                _id: mongodb.ObjectID(params.id)
-            },
-                {
-                    title: payLoad.title,
-                    description: payLoad.description,
-                });
-            return userData;
-        })
-    } catch (error) {
-        console.error(error)
-        throw error
-
-    }
-
-}
-
-//Update notice
-
-const updateNotice = async (payLoad, params) => {
-    console.log("Update notice")
-    try {
-        console.log("updateNotice")
-        notice.findOne({ id: params.id }, async (err, data) => {
-
-            console.log('111000')
-            let userData = await notice.updateOne({
-                _id: mongodb.ObjectID(params.id)
-            },
-                {
-                    title: payLoad.title,
-                    description: payLoad.description,
-                });
-            return userData;
-        })
-    } catch (error) {
-        console.error(error)
-        throw error
-
-    }
-
-}
-
-//Delete announcement
-
-const deleteAnnouncement = async (params) => {
-    console.log("Delete announcement")
-    try {
-        console.log("deleteAnnouncement")
-        let userData = await announcement.deleteOne({ _id: mongodb.ObjectID(params.id) });
-        return userData;
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
-}
-
-//Delete notice
-
-const deleteNotice = async (params) => {
-    console.log("Delete notice")
-    try {
-        console.log("deleteNotice")
-        let userData = await notice.deleteOne({ _id: mongodb.ObjectID(params.id) });
-        return userData;
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
-}
-module.exports = { userAnnouncement, userNotice, userEmail, sendMail, getAnnouncement, getNotice, updateAnnouncement, updateNotice, deleteAnnouncement, deleteNotice }
+    module.exports = { userEmail, sendMail }
--- validators/userValidator.js
+++ validators/userValidator.js
@@ -1,26 +1,10 @@
 const { Joi, celebrate } = require('celebrate')
-
-const announcementValidator = celebrate({
-    body: Joi.object().keys({
-        title:Joi.string().required(),
-        description:Joi.string().required(),
-    })
-})
-
-const noticeValidator = celebrate({
-    body: Joi.object().keys({
-        title:Joi.string().required(),
-        description:Joi.string().required(),
-    })
-})
-
-
 const emailValidator = celebrate({
     body: Joi.object().keys({
         name:Joi.string().required(),
-        email:Joi.string().required(),
+        email:Joi.string().email().required(),
         subject:Joi.string().required(),
         message:Joi.string().required(),
     })
 })
-module.exports = { announcementValidator, noticeValidator, emailValidator}
+module.exports = {emailValidator}
